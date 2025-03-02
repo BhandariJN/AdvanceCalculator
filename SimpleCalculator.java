61bffb8883a373f270e3c742d02fdf6ae2594098
ghale123 <ghaleaangkami08@gmail.com>
2025-03-02 08:52:22 +0545

    final ui

--- SimpleCalculator.java
+++ SimpleCalculator.java
@@ -1,10 +1,16 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JRadioButton;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
-public class SimpleCalculator {
+public class SimpleCalculator implements ActionListener{
 
  double number, answer;
     int calculation;
@@ -35,6 +41,154 @@ public class SimpleCalculator {
     JButton buttonSquare = new JButton("x\u00B2");
     JButton buttonReciprocal = new JButton("1/x");
     JButton buttonSqrt = new JButton("\u221A");
+    public void prepareGUI() {
+        frame = new JFrame();
+        frame.setTitle("Calculator");
+        frame.setSize(300, 490);
+        frame.getContentPane().setLayout(null);
+        frame.getContentPane().setBackground(Color.BLACK);
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public void addComponents() {
+        label.setBounds(250, 0, 50, 50);
+        label.setForeground(Color.white);
+        frame.add(label);
+
+        textField.setBounds(10, 40, 270, 40);
+        textField.setFont(new Font("Arial", Font.BOLD, 20));
+        textField.setEditable(false);
+        textField.setHorizontalAlignment(SwingConstants.RIGHT);
+        frame.add(textField);
+
+        
+        buttonSeven.setBounds(10, 230, 60, 40);
+        buttonSeven.setFont(new Font("Arial", Font.BOLD, 20));
+        frame.add(buttonSeven);
+
+        buttonEight.setBounds(80, 230, 60, 40);
+        buttonEight.setFont(new Font("Arial", Font.BOLD, 20));
+        frame.add(buttonEight);
+
+        buttonNine.setBounds(150, 230, 60, 40);
+        buttonNine.setFont(new Font("Arial", Font.BOLD, 20));
+        frame.add(buttonNine);
+
+        buttonFour.setBounds(10, 290, 60, 40);
+        buttonFour.setFont(new Font("Arial", Font.BOLD, 20));
+        frame.add(buttonFour);
+
+        buttonFive.setBounds(80, 290, 60, 40);
+        buttonFive.setFont(new Font("Arial", Font.BOLD, 20));
+        frame.add(buttonFive);
+
+        buttonSix.setBounds(150, 290, 60, 40);
+        buttonSix.setFont(new Font("Arial", Font.BOLD, 20));
+        frame.add(buttonSix);
+
+        buttonOne.setBounds(10, 350, 60, 40);
+        buttonOne.setFont(new Font("Arial", Font.BOLD, 20));
+        frame.add(buttonOne);
+
+        buttonTwo.setBounds(80, 350, 60, 40);
+        buttonTwo.setFont(new Font("Arial", Font.BOLD, 20));
+        frame.add(buttonTwo);
+
+        buttonThree.setBounds(150, 350, 60, 40);
+        buttonThree.setFont(new Font("Arial", Font.BOLD, 20));
+        frame.add(buttonThree);
+
+        buttonDot.setBounds(150, 410, 60, 40);
+        buttonDot.setFont(new Font("Arial", Font.BOLD, 20));
+        frame.add(buttonDot);
+
+        buttonZero.setBounds(10, 410, 130, 40);
+        buttonZero.setFont(new Font("Arial", Font.BOLD, 20));
+        frame.add(buttonZero);
+
+        buttonEqual.setBounds(220, 350, 60, 100);
+        buttonEqual.setFont(new Font("Arial", Font.BOLD, 20));
+        buttonEqual.setBackground(new Color(239, 188, 2));
+        frame.add(buttonEqual);
+
+        buttonDiv.setBounds(220, 110, 60, 40);
+        buttonDiv.setFont(new Font("Arial", Font.BOLD, 20));
+        buttonDiv.setBackground(new Color(239, 188, 2));
+        frame.add(buttonDiv);
+
+        buttonSqrt.setBounds(10, 170, 60, 40);
+        buttonSqrt.setFont(new Font("Arial", Font.BOLD, 18));
+        frame.add(buttonSqrt);
+
+        buttonMul.setBounds(220, 230, 60, 40);
+        buttonMul.setFont(new Font("Arial", Font.BOLD, 20));
+        buttonMul.setBackground(new Color(239, 188, 2));
+        frame.add(buttonMul);
+
+        buttonMinus.setBounds(220, 170, 60, 40);
+        buttonMinus.setFont(new Font("Arial", Font.BOLD, 20));
+        buttonMinus.setBackground(new Color(239, 188, 2));
+        frame.add(buttonMinus);
+
+        buttonPlus.setBounds(220, 290, 60, 40);
+        buttonPlus.setFont(new Font("Arial", Font.BOLD, 20));
+        buttonPlus.setBackground(new Color(239, 188, 2));
+        frame.add(buttonPlus);
+
+        buttonSquare.setBounds(80, 170, 60, 40);
+        buttonSquare.setFont(new Font("Arial", Font.BOLD, 20));
+        frame.add(buttonSquare);
+
+        buttonReciprocal.setBounds(150, 170, 60, 40);
+        buttonReciprocal.setFont(new Font("Arial", Font.BOLD, 15));
+        frame.add(buttonReciprocal);
+
+        buttonDelete.setBounds(150, 110, 60, 40);
+        buttonDelete.setFont(new Font("Arial", Font.BOLD, 12));
+        buttonDelete.setBackground(Color.red);
+        buttonDelete.setForeground(Color.white);
+        frame.add(buttonDelete);
+
+        buttonClear.setBounds(80, 110, 60, 40);
+        buttonClear.setFont(new Font("Arial", Font.BOLD, 12));
+        buttonClear.setBackground(Color.red);
+        buttonClear.setForeground(Color.white);
+        frame.add(buttonClear);
+
+    }
+
+
+    public void addActionEvent() {
+        onRadioButton.addActionListener(this);
+        offRadioButton.addActionListener(this);
+        buttonClear.addActionListener(this);
+        buttonDelete.addActionListener(this);
+        buttonDiv.addActionListener(this);
+        buttonSqrt.addActionListener(this);
+        buttonSquare.addActionListener(this);
+        buttonReciprocal.addActionListener(this);
+        buttonMinus.addActionListener(this);
+        buttonSeven.addActionListener(this);
+        buttonEight.addActionListener(this);
+        buttonNine.addActionListener(this);
+        buttonMul.addActionListener(this);
+        buttonFour.addActionListener(this);
+        buttonFive.addActionListener(this);
+        buttonSix.addActionListener(this);
+        buttonPlus.addActionListener(this);
+        buttonOne.addActionListener(this);
+        buttonTwo.addActionListener(this);
+        buttonThree.addActionListener(this);
+        buttonEqual.addActionListener(this);
+        buttonZero.addActionListener(this);
+        buttonDot.addActionListener(this);
+
+
+    }
+
 
 
 
@@ -44,6 +198,12 @@ public class SimpleCalculator {
         new SimpleCalculator();
         System.out.println("Program Runned");
 
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+        throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
     }
 
 }
